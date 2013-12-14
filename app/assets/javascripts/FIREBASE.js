39c3a840507cc36a2c2feb4801dba3b69a1b404a
lsugino <lindseysugino@gmail.com>
2013-12-14 10:21:44 -0800

    name now stored as private variable
    - removes name variable from object literal

--- app/assets/javascripts/FIREBASE.js
+++ app/assets/javascripts/FIREBASE.js
@@ -6,14 +6,16 @@ var Sync = (function() {
 
   var database = new Firebase('https://fb-squares.firebaseio.com/');
   var room = window.location.href.match(/\/([\w-]+)(?=\/signup)/)[0]
+  var username
 
   return {
     addUserToFirebase: function(name) {
-      this.name = name;
+      username = name;
       database.child(room).child(name).set( {'payment': 'unpaid', 'locations' : 'none'} );
     },
     assignCell: function(row, col) {
-      database.child(room).child(this.name).child('locations').child(row + '-' + col).set('true')
+      debugger
+      database.child(room).child(username).child('locations').child(row + '-' + col).set('true')
     },
   }
 
