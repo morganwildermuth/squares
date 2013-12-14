9d134e81ac307e3ab3a0869ebbb17e44071091f0
Nicholas Scricco <nicholasscricco@Nicholass-MacBook-Air.local>
2013-12-13 23:27:55 -0800

    decoupled and refactored javascript to submit settings

--- app/assets/javascripts/landing-page.js
+++ app/assets/javascripts/landing-page.js
@@ -1,1 +1,39 @@
-console.log('js loaded')
+var handleSubmitClick = function(){
+	$('.create-board').on('click', function(){
+		if($('.board-name').attr('hidden') === 'hidden'){
+			showSettingInputs()
+		} else {
+			createBoard(getSettingInputs())
+		}
+	})
+}
+
+var showSettingInputs = function(){
+	$('.board-name').removeAttr('hidden')
+	$('.square-price').removeAttr('hidden')
+}
+
+var getSettingInputs = function(){
+	return {
+		boardName: $('.board-name').val(),
+		squarePrice: $('.square-price').val()
+	}
+}
+
+var sync = {
+	init: function(){
+		var ref = new Firebase('https://fb-squares.firebaseIO.com')
+		this.board = ref.push()
+	}
+}
+
+var createBoard = function(attrs){
+	sync.init()
+
+	sync.board.child('name').set(attrs.boardName)
+	sync.board.child('price').set(attrs.squarePrice)
+}
+
+$(document).ready(function(){
+	handleSubmitClick()
+})
