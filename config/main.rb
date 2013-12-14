69c9c2b75d1466f7ba97f5995c14fc62239275b7
lsugino <lindseysugino@gmail.com>
2013-12-13 21:48:08 -0800

    configure sinatra to match file structure
    - root set to folder structure
    - views set to app/views
    - public_folder set to app/assets

--- config/main.rb
+++ config/main.rb
@@ -1,3 +1,7 @@
 $LOAD_PATH.unshift(File.expand_path('.'))
 
 require 'sinatra'
+
+set :root, File.dirname(File.dirname(__FILE__))
+set :views, Proc.new { File.join(root, "app/views") }
+set :public_folder, Proc.new { File.join(root, "app/assets") }
