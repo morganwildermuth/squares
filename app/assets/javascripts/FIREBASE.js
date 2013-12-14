1b00ed2f697f5fb80d47092e7fb23596758337b2
lsugino <lindseysugino@gmail.com>
2013-12-14 02:22:14 -0800

    god commit

--- app/assets/javascripts/FIREBASE.js
+++ app/assets/javascripts/FIREBASE.js
@@ -1,17 +1,29 @@
 $( document ).ready(function() {
   addSubmitListener()
+  // Sync.addChangeCallback()
 })
-
 
 var Sync = (function() {
 
   var database = new Firebase('https://fb-squares.firebaseio.com/');
   var room = window.location.href.match(/\/([\w-]+)(?=\/signup)/)[0]
 
+  // var getCells = function(database) {
+  //   return 'this is working'
+  // };
+
   return {
     addUserToFirebase: function(name) {
+      this.name = name;
       database.child(room).child(name).set( {'payment': 'unpaid', 'locations' : 'none'} );
-    }
+    },
+    assignCell: function(row, col) {
+      database.child(room).child(this.name).child('locations').child(row + '-' + col).set('true')
+    },
+    // addChangeCallback: function() {
+    //   var cellArray = getCells(database)
+    //   database.on('child_added',Board.updateCells)
+    // }
   }
 
 })()
